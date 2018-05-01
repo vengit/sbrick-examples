3c5900c7287a42e606fd7ca8f78aa6293a799634
Fábián Tamás László <giganetom@gmail.com>
2018-05-01 14:11:26 +0200

    Examples

---
+++ microbit/pf-accelerometer-car.js
@@ -0,0 +1,47 @@
+let lamp = 0
+bluetooth.onBluetoothConnected(() => {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+})
+bluetooth.onBluetoothDisconnected(() => {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+})
+sbrick.onConnected(() => {
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . . # . .
+        . . . # .
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.A, () => {
+    lamp = 1 - lamp
+    sbrick.drive(lamp * 255, 1, 0)
+})
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+sbrick.connect("SBrick2")
+lamp = 0
+basic.forever(() => {
+    sbrick.driveFromAccel(3, 1)
+    basic.pause(50)
+    sbrick.driveFromAccel(2, 0)
+    basic.pause(50)
+})
